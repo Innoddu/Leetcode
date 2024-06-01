06afe86ef6961d5d1165d5b85d9cbc34e4a16604
innoddu <ciui9494@gmail.com>
2024-05-31 18:19:40 -0700

    Add four files

---
+++ greedy/hands_of_straights.py
@@ -0,0 +1,25 @@
+# Hand of Straights
+import heapq
+
+def isNStraightHand(hand, groupSize):
+        if len(hand) % groupSize > 0:
+            return False
+
+        count = {}
+        for n in hand:
+            count[n] = 1 + count.get(n, 0)
+        
+        minHeap = list(count.keys())
+        heapq.heapify(minHeap)
+        while minHeap:
+            first = minHeap[0]
+            for i in range(first, first + groupSize):
+                if i not in count:
+                    return False
+                count[i] -= 1
+                if count[i] == 0:
+                    if i != minHeap[0]:
+                        return False
+                    heapq.heappop(minHeap)
+        
+        return True
---
+++ greedy/jump_game_2.py
@@ -0,0 +1,13 @@
+# 45. Jump Game II
+def jump(nums):
+        cnt = 0
+        l = r = 0
+
+        while r < len(nums) - 1:
+            farthest = 0
+            for i in range(l, r + 1):
+                farthest = max(farthest, i + nums[i])
+            l = r + 1
+            r = farthest
+            cnt += 1
+        return cnt
